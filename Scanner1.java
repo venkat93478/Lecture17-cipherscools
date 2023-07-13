fb68205175d319babb73696ced17e71f01dfbc4e
Venkata sai <116934116+venkat93478@users.noreply.github.com>
2023-07-13 21:29:20 +0530

    Create Scanner1.java

---
+++ Scanner1.java
@@ -0,0 +1,22 @@
+package Arrays;
+import java.util.Scanner;
+
+public class ScannerClass {
+	
+	  public static void main(String[] args) {
+		  
+	    Scanner myObj = new Scanner(System.in);
+
+	    System.out.println("Enter name, age and salary:");
+	    
+	    String name = myObj.nextLine();
+
+	    int age = myObj.nextInt();
+	    
+	    double salary = myObj.nextDouble();
+	    
+	    System.out.println("Name: " + name);
+	    System.out.println("Age: " + age);
+	    System.out.println("Salary: " + salary);
+	  }
+	}
